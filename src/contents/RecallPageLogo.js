af2f5582f48f1dfcd92a3bd1ae98ad3f7726b909
Luana Lessa <lessalsn@gmail.com>
2022-03-28 20:13:48 -0300

    chore: Code review

--- src/contents/RecallPageLogo.js
+++
@@ -1,8 +0,0 @@
-export default function RecallPageLogo(){
-    return (
-        <div className={`logo-pequeno`}>
-            <img src="assets/logo-pequeno.png" className="logo"/>
-            <h1>ZapRecall</h1>
-        </div>
-    )
-}
